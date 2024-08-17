b5c1d7eb26407118466c4ac014ab4ca36dad58fc
charleshuy <pathofdoomz@gmail.com>
2024-08-17 14:59:26 +0700

    Fixing some typo

--- SewingFactory.Services/Service/UserService.cs
+++ SewingFactory.Services/Service/UserService.cs
@@ -16,7 +16,7 @@ namespace SewingFactory.Services.Service
         private readonly IConfiguration _configuration;
         private readonly ITokenService _tokenService;
 
-        public UserSService(DatabaseContext dbContext, IConfiguration configuration, ITokenService tokenService)
+        public UserService(DatabaseContext dbContext, IConfiguration configuration, ITokenService tokenService)
         {
             _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
