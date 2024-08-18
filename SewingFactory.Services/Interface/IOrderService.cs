3b40e1d0fc179300aa2f696b3d3754c9784b20c6
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-08-18 22:27:02 +0700

    Setup

---
+++ SewingFactory.Services/Interface/IOrderService.cs
@@ -0,0 +1,30 @@
+﻿using SewingFactory.Models;
+using SewingFactory.Models.DTO;
+
+namespace SewingFactory.Services.Interface
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetOrderList();
+
+        Task<Order> GetOrder(Guid orderID);
+
+        Task<GetOrderDTO> GetOrderDTO(Guid orderID);
+
+        Task<IEnumerable<GetOrderDTO>> GetAllOrderDTOList();
+
+        Task<bool> IsValidOrder(Guid orderID);
+
+        Task<bool> AddOrder(AddOrderDTO dto);
+
+        Task<bool> UpdateOrder(UpdateOrderDTO dto);
+
+        Task<bool> IsValidUserForAddOrderFeature(Guid userId);
+
+        Task<bool> IsValidUserForUpdateOrderFeature(Guid userId);
+
+        bool IsValidStatusFormat(string? status);
+
+        Task<string?> IsGenerallyValidated(Guid productID, int? quantity, string? CustomerName, string? CustomerPhone);
+    }
+}
